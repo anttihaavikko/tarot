20076cb46a35e4cc380866dc0657f2e07b064f08
Antti Haavikko <antti.haavikko@gmail.com>
2022-11-30 15:03:00 +0200

    Change build settings etc

--- pushbuilds.sh
+++ pushbuilds.sh
@@ -3,14 +3,14 @@ NC='\033[0m' # No Color
 
 echo " "
 
-# echo "${COLOR}Pushing build for Windows${NC}"
-# butler push Builds/win anttihaavikko/in-the-cards:win --fix-permissions
+echo "${COLOR}Pushing build for Windows${NC}"
+butler push Builds/win anttihaavikko/in-the-cards:win --fix-permissions
 
-# echo "${COLOR}Pushing build for OSX${NC}"
-# butler push Builds/osx anttihaavikko/in-the-cards:osx --fix-permissions
+echo "${COLOR}Pushing build for OSX${NC}"
+butler push Builds/osx anttihaavikko/in-the-cards:osx --fix-permissions
 
-# echo "${COLOR}Pushing build for Linux${NC}"
-# butler push Builds/linux anttihaavikko/in-the-cards:linux
+echo "${COLOR}Pushing build for Linux${NC}"
+butler push Builds/linux anttihaavikko/in-the-cards:linux
 
 # echo "${COLOR}Copying html5 files over to correct path"
 # cp -a Builds/webgl/html5/Build/. Builds/html5/Build
